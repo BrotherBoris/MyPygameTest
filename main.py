ea54663aa9133ebeb3bd044b6cf4ec4d4a92d3b8
unknown <joaoantoniofer@gmail.com>
2022-07-12 06:05:26 -0300

    Small tests 12/07/22

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import random
 import pygame
+import sys
 
-print(random.randint(0, 2))
+screensize = (800,600)
+gameScreen = pygame.display.set_mode(screensize)
+pygame.display.set_caption("Game")
+pygame.init()
+
+wheelRotation = 0
+  
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEWHEEL:
+            wheelRotation +=1
+            print(wheelRotation)
+
+        if event.type == pygame.MOUSEMOTION:
+            x, y = pygame.mouse.get_pos()
+            print(x, y)
+            if x > 200 and x<600 :
+                pygame.mouse.set_pos(601,y)
+            
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            
+            exit()
+            
+    pygame.display.update()    
+    
